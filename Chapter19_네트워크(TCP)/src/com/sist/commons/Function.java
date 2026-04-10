24860224f4eaf88b180113551467053765872223
skc4234 <skc4234@naver.com>
2026-04-10 17:46:59 +0900

    04-10 Socket, ServerSocket으로 채팅 프로그램 만들기

--- Chapter19_네트워크(TCP)/src/com/sist/commons/Function.java
+++ Chapter19_네트워크(TCP)/src/com/sist/commons/Function.java
@@ -13,6 +13,28 @@ package com.sist.commons;
  *     
  *     
  */
+/*
+ * 자바 공부법
+ *  - 형식, 문법, 사용처
+ *  1. 메소드
+ *  2. 제어문
+ *  3. 클래스 => 변수, 메소드, 생성자
+ *  4. 캡슐화
+ *  5. 추상클래스 / *인터페이스
+ *  6. 예외처리
+ *  ----------------------------
+ *  7. 라이브러리
+ *     1) String / Collection(List, Map)
+ *     2) IO stream
+ *  -----------------------------
+ *  2차 자바 => J2EE(JSP)
+ *  3차 자바 => Spring
+ *  4차 자바 => MyBatis / JPA
+ *  5차 자바 => SpringBoot
+ *  -----------------------------
+ *  구조는 변경 x
+ * 
+ */
 public class Function {
 	public static final int LOGIN = 100;
 	public static final int MYLOG = 110; // 로그인 : 이미 로그인 된 사람 / 로그인 한 사람
